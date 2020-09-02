c6cb798b53f34f033013a42e831c5edb915820e6
Kalle Raiskila <kraiskil@iki.fi>
2020-09-03 01:08:37 +0300

    Generic Onnx::Attribute-parsing utilities.

---
+++ src/util.cc
@@ -0,0 +1,70 @@
+#include "error.h"
+#include "util.h"
+
+std::string cify_name(const std::string &in)
+{
+	// Replace all non-allowed characters with underscore
+	std::string rv = in;
+	for( auto &c : rv )
+	{
+		// TODO: Check what the C standard allows. isalnum takes locales into account
+		if( isalnum(c) )
+			continue;
+		c='_';
+	}
+
+	return rv;
+}
+
+
+int parse_attribute_int(const onnx::AttributeProto &a)
+{
+	if( a.has_i() == false )
+		ERROR("Not a int attribute");
+
+	return a.i();
+}
+
+float parse_attribute_float(const onnx::AttributeProto &a)
+{
+	if( a.has_f() == false )
+		ERROR("Not a float attribute");
+
+	return a.f();
+}
+std::vector<float> parse_attribute_floats(const onnx::AttributeProto &a)
+{
+	if( a.floats_size() == 0 )
+		ERROR("Not a floats attribute");
+
+	std::vector<float> rv;
+
+	for( float f : a.floats() )
+		rv.push_back(f);
+
+	return rv;
+}
+
+std::string parse_attribute_string(const onnx::AttributeProto &a)
+{
+	if( a.has_s() == false )
+		ERROR("Not a string attribute");
+
+	return a.s();
+}
+
+std::vector<std::string> parse_attribute_strings(const onnx::AttributeProto &a)
+{
+	if( a.strings_size() == 0 )
+		ERROR("Not a floats attribute");
+
+	std::vector<std::string> rv;
+
+	for( auto s : a.strings() )
+		rv.push_back(s);
+
+	return rv;
+
+
+}
+
--- src/util.h
+++ src/util.h
@@ -1,18 +1,15 @@
 #pragma once
 #include <string>
-/* ONNX names are not valid C. C-ify them */
-static std::string cify_name(const std::string &in)
-{
-	// Replace all non-allowed characters with underscore
-	std::string rv = in;
-	for( auto &c : rv )
-	{
-		// TODO: Check what the C standard allows. isalnum takes locales into account
-		if( isalnum(c) )
-			continue;
-		c='_';
-	}
+#include <vector>
+#include "onnx.pb.h"
 
-	return rv;
-}
+/* ONNX names are not valid C - make name acceptable to the C compiler*/
+std::string cify_name(const std::string &in);
+
+/* Helper functions to parse attributes in a onnx NodeProto */
+int parse_attribute_int(const onnx::AttributeProto &a);
+float parse_attribute_float(const onnx::AttributeProto &a);
+std::vector<float> parse_attribute_floats(const onnx::AttributeProto &a);
+std::string parse_attribute_string(const onnx::AttributeProto &a);
+std::vector<std::string> parse_attribute_strings(const onnx::AttributeProto &a);
 
