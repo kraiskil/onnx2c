f14c479887e594fb8e08a790b91f9519a24cbce9
Kalle Raiskila <kraiskil@iki.fi>
2020-10-27 15:02:37 +0200

    Reshape: implement dimension sizes 0 and -1

--- src/nodes/reshape.h
+++ src/nodes/reshape.h
@@ -29,6 +29,7 @@ class Reshape : public Node {
 
 		/* TODO: is there ANY case where a reshape needs to re-order the internal data layout ? */
 		/* TODO: and if not - check that at least gcc can get rid of this copy! (So onnx2c doesn't need to) */
+		/* TODO: or - can we mark output an onnx2c-alias of input? */
 		dst << "\t/*Reshape*/" << std::endl;
 		dst << "\t" << type << " *data = (" << type << "*)" << data->cname() << ";" << std::endl;
 		dst << "\t" << type << " *reshaped = (" << type << "*)" << reshaped->cname() << ";" << std::endl;
@@ -37,42 +38,60 @@ class Reshape : public Node {
 		dst << "\t\treshaped[i] = data[i];" << std::endl;
 		dst << std::endl;
 	}
- 
+
 	virtual void resolveOutput(const std::vector< const Tensor*> &inputs, std::vector<Tensor *> &outputs) override
 	{
 		data = inputs[0];
 		shape = inputs[1];
 		if( typeConstraint_int64(shape) == false )
-			ERROR("Incorrect input for node"); 
+			ERROR("Incorrect input for node");
 
 
 		if( shape->initialize == false ) {
 			ERROR("Reshaping to a run-time defined shape is not supported");
 		}
 
+		std::vector<int> out_data_dim;
 		int64_t *new_shape = (int64_t*)(shape->data_buffer);
+		bool negative_shape_found=false;
+		int negative_shape_at = -1;
 
-		/* TODO: uint64_t does not overflow here, right?
-		 * Because if it does, you probably aren't targetting edge computing? */
-		uint64_t shape_num_elem=1;
-		for(int i=0; i<shape->data_num_elem(); i++) {
-			// dimension=0: unchanged from input
-			// dimension=-1: infer from dim
-			if( new_shape[i] <= 0 )
-				ERROR("Unimplemented: reshaping to shapes 0 or -1");
+		/* Check new shape has no more than 1 negative. Replace zeros */
+		uint64_t output_size=1;
+		for(unsigned i=0; (int)i<shape->data_num_elem(); i++) {
+			int s = new_shape[i];
+			if( s < 0 ) {
+				if( negative_shape_found )
+					ERROR("Bad input: two negatives in reshape's target shape");
+				else {
+					negative_shape_found = true;
+					negative_shape_at=i;
+				}
+			}
+			else if( s == 0 ) {
+				if( i >= data->data_dim.size() )
+					ERROR("Bad input: Reshape request duplication of input dimension that don't exist");
+				s=data->data_dim[i];
+			}
 
-			shape_num_elem *= new_shape[i];
+			out_data_dim.push_back(s);
+
+			if( s > 0 )
+				output_size *= s;
 		}
 
-		if( shape_num_elem != (uint64_t)data->data_num_elem() )
-			ERROR("Reshape: Wrong amount of input for requested shape");
+		if( negative_shape_found ) {
+			int missing_dim = data->data_num_elem() / output_size;
+			// If these don't match, the input is wrong.
+			if( output_size * missing_dim != (uint64_t)data->data_num_elem() )
+				ERROR("Could not deduce implicit dimension size for Resize node");
+
+			out_data_dim[negative_shape_at] = missing_dim;
+		}
+
 
 		Tensor *rv = new Tensor;
-		// TODO: what if B has more dimensions? Check ONNX semantics
-		for( int i=0; i<shape->data_num_elem(); i++) {
-			int64_t d = new_shape[i];
-			rv->data_dim.push_back(d);
-		}
+		rv->data_dim = out_data_dim;
 
 		rv->data_type = data->data_type;
 		reshaped = rv;
