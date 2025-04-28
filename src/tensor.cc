26e552c399bfea6bd209a3803131ebecfedc2c9e
Felix <166597450+FeKrtz@users.noreply.github.com>
2025-04-28 09:35:36 +0200

    Update src/tensor.cc
    Co-authored-by: kraiskil <kraiskil@users.noreply.github.com>

--- src/tensor.cc
+++ src/tensor.cc
@@ -228,7 +228,6 @@ void Tensor::print_element(std::ostream &dst, uint64_t element) const
 		case onnx::TensorProto_DataType_DOUBLE:
 		{
 			double *f = static_cast<double*>(data_buffer);
-			//dst << std::showpoint << f[element]<< "f";
 			dst << std::fixed << f[element]<< "f";
 			break;
 		}
