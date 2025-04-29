eba2f3a33dbfa23e8ae854917ba7b13f554ff716
Felix Kreutz <felix.kreutz@infineon.com>
2025-04-29 08:00:55 +0000

    use sdt:fixed for print floats

--- src/tensor.cc
+++ src/tensor.cc
@@ -222,15 +222,13 @@ void Tensor::print_element(std::ostream &dst, uint64_t element) const
 			The test passes if std::fixed is used printing 479001600.000000
 			*/
 			float *f = static_cast<float*>(data_buffer);
-			dst << std::showpoint << f[element]<< "f";
-			// dst << std::fixed << f[element]<< "f";
+			dst << std::fixed << f[element]<< "f";
 			break;
 		}
 		case onnx::TensorProto_DataType_DOUBLE:
 		{
 			double *f = static_cast<double*>(data_buffer);
-			dst << std::showpoint << f[element]<< "f";
-			// dst << std::fixed << f[element]<< "f";
+			dst << std::fixed << f[element]<< "f";
 			break;
 		}
 		case onnx::TensorProto_DataType_INT8:
