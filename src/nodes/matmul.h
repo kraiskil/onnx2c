cdb1a6be8f499a6a7fef57fc0e7fe092ac5fd373
Can <can.lehmann@emmtrix.com>
2025-10-01 18:02:02 +0200

    Local node tests and implement

--- src/nodes/matmul.h
+++ src/nodes/matmul.h
@@ -27,54 +27,37 @@ void MatMul::resolve(void) {
 	name_input(0, "A");
 	name_input(1, "B");
 
-	std::vector<int> a_dim = a->data_dim;
-	if (a_dim.size() == 1) {
-		a_dim.insert(a_dim.begin(), 1);
-	} else if (a_dim.size() == 0) {
-		ERROR("Scalar input not supported for MatMul");
-	}
-
-	std::vector<int> b_dim = b->data_dim;
-	if (b_dim.size() == 1) {
-		b_dim.push_back(1);
-	} else if (a_dim.size() == 0) {
-		ERROR("Scalar input not supported for MatMul");
-	}
-
-	assert(a_dim.size() >= 2);
-	assert(b_dim.size() >= 2);
-
-	for (unsigned i = 2; i < a_dim.size() && i < b_dim.size(); i++) {
-		if (a_dim[a_dim.size() - i - 1] != b_dim[b_dim.size() - i - 1]) {
-			ERROR("Invalid broadcast dimensions for MatMul");
-		}
-	}
+	assert(a->rank() >= 1);
+	assert(b->rank() >= 1);
 
 	std::vector<int> y_dim;
 
-	if (a_dim.size() > b_dim.size()) {
-		y_dim.insert(y_dim.end(), a_dim.begin(), a_dim.end() - 2);
-	} else {
-		y_dim.insert(y_dim.end(), b_dim.begin(), b_dim.end() - 2);
+	if (a->rank() > 2 || b->rank() > 2) {
+		for (unsigned i = 2; i < a->rank() && i < b->rank(); i++) {
+			if (a->data_dim[a->rank() - i - 1] != b->data_dim[b->rank() - i - 1]) {
+				ERROR("Invalid broadcast dimensions for MatMul");
+			}
+		}
+
+		if (a->data_dim.size() > b->data_dim.size()) {
+			y_dim.insert(y_dim.end(), a->data_dim.begin(), a->data_dim.end() - 2);
+		} else {
+			y_dim.insert(y_dim.end(), b->data_dim.begin(), b->data_dim.end() - 2);
+		}
 	}
 	
-	y_dim.push_back(a_dim[a_dim.size() - 2]);
-	y_dim.push_back(b_dim[b_dim.size() - 1]);
+	if (a->rank() > 1) {
+		y_dim.push_back(a->data_dim[a->rank() - 2]);
+	}
+	
+	if (b->rank() > 1) {
+		y_dim.push_back(b->data_dim[b->rank() - 1]);
+	}
 
 	Tensor *y = new Tensor;
 	y->data_dim = y_dim;
 	y->data_type = a->data_type;
 	register_output(y, "Y");
-}
-
-static std::string matmul_broadcast(std::string name, const Tensor *t, int to_rank) {
-	assert(t->rank() > 0);
-	std::ostringstream dst;
-	dst << name;
-	for (int i = 0; i < (int)t->data_dim.size() - 2; i++) {
-		dst << "[i" << (to_rank - t->data_dim.size() + i) << "]";
-	}
-	return dst.str();
 }
 
 void MatMul::print(std::ostream &dst) const {
@@ -84,34 +67,70 @@ void MatMul::print(std::ostream &dst) const {
 	Tensor *b = get_input_tensor(1);
 	Tensor *y = get_output_tensor(0);
 
-	for (int i = 0; i < (int)y->rank() - 2; i++) {
+	// Number of dimensions to broadcast over
+	int broadcast_dims = y->rank();
+	if (a->rank() > 1) {
+		broadcast_dims--;
+	}
+	if (b->rank() > 1) {
+		broadcast_dims--;
+	}
+
+	for (int i = 0; i < broadcast_dims; i++) {
 		std::string lv = "i" + std::to_string(i);
 		INDT_1 << "for (unsigned " << lv << "=0; " << lv << "<" << y->data_dim[i] << "; " << lv << "++)" << std::endl;
 	}
 
-	std::string a_idx = matmul_broadcast("A", a, y->rank());
-	if (a->data_dim.size() == 1) {
+	std::string a_idx = "A";
+	if (a->rank() == 1) {
 		a_idx += "[k]";
 	} else {
+		for (int i = 0; i < (int)a->rank() - 2; i++) {
+			a_idx += "[i" + std::to_string(broadcast_dims - ((int)a->rank() - 2) + i) + "]";
+		}
 		a_idx += "[i][k]";
 	}
 
-	std::string b_idx = matmul_broadcast("B", b, y->rank());
-	if (b->data_dim.size() == 1) {
+	std::string b_idx = "B";
+	if (b->rank() == 1) {
 		b_idx += "[k]";
 	} else {
+		for (int i = 0; i < (int)b->rank() - 2; i++) {
+			b_idx += "[i" + std::to_string(broadcast_dims - ((int)b->rank() - 2) + i) + "]";
+		}
 		b_idx += "[k][j]";
 	}
 
-	std::string y_idx = matmul_broadcast("Y", y, y->rank()) + "[i][j]";
+	std::string y_idx;
+	if (y->is_scalar()) {
+		y_idx = "*Y";
+	} else {
+		y_idx = "Y";
+
+		for (int i = 0; i < broadcast_dims; i++) {
+			y_idx += "[i" + std::to_string(i) + "]";
+		}
+
+		if (a->rank() > 1) {
+			y_idx += "[i]";
+		}
+
+		if (b->rank() > 1) {
+			y_idx += "[j]";
+		}
+	}
+
+	int i_dim = (a->rank() > 1) ? a->data_dim[a->rank() - 2] : 1;
+	int j_dim = (b->rank() > 1) ? b->data_dim[b->rank() - 1] : 1;
+	int k_dim = a->data_dim[a->rank() - 1];
 
 	INDT_1 << "{" << std::endl;
 	
-	INDT_2 << "for (unsigned i = 0; i < " << y->data_dim[y->rank() - 2] << "; i++)" << std::endl;
-	INDT_2 << "for (unsigned j = 0; j < " << y->data_dim[y->rank() - 1] << "; j++)" << std::endl;
+	INDT_2 << "for (unsigned i = 0; i < " << i_dim << "; i++)" << std::endl;
+	INDT_2 << "for (unsigned j = 0; j < " << j_dim << "; j++)" << std::endl;
 	INDT_2 << "{" << std::endl;
-	INDT_2 << y_idx << " = 0;" << std::endl;
-	INDT_3 << "for (unsigned k = 0; k < " << a->data_dim[a->rank() - 1] << "; k++)" << std::endl;
+	INDT_3 << y_idx << " = 0;" << std::endl;
+	INDT_3 << "for (unsigned k = 0; k < " << k_dim << "; k++)" << std::endl;
 	INDT_4 << y_idx << " += " << a_idx << " * " << b_idx << ";" << std::endl;
 	INDT_2 << "}" << std::endl;
 
