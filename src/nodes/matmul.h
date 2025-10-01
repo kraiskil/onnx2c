4dd53bce0039a10fffb402eb857f811e3310fc67
Can <can.lehmann@emmtrix.com>
2025-10-01 15:33:56 +0200

    Implement all MatMul broadcasting rules

--- src/nodes/matmul.h
+++ src/nodes/matmul.h
@@ -1,3 +1,10 @@
+/* This file is part of onnx2c.
+ *
+ * MatMul node.
+ * 
+ */
+
+#include "node.h"
 
 namespace toC {
 
@@ -7,148 +14,110 @@ class MatMul : public Node {
 		op_name = "MatMul";
 	}
 
-	std::string vecstr( const std::vector<int>& vec ) const
-	{
-		std::stringstream result;
-		result << "{ ";
-		std::copy( vec.begin(), vec.end(), std::ostream_iterator<int>( result, ", " ) );
-		result << "}";
-		return result.str();
-	}
-
-	virtual void print(std::ostream &dst) const override
-	{
-		const Tensor *A = get_input_tensor(0);
-		const Tensor *B = get_input_tensor(1);
-		std::string type = A->data_type_str();
-
-		bool A_is_correct_size = A->data_dim.size() == 2 || A->data_dim.size() == 3;
-		bool B_is_correct_size = B->data_dim.size() == 2 || B->data_dim.size() == 3;
-		if ( !A_is_correct_size || !B_is_correct_size )
-		{
-			ERROR( std::string( "Unimplemented: MatMul with dimensions: A: " ) + vecstr( A->data_dim ) + ", B: " + vecstr( B->data_dim ) );
-		}
-
-		std::vector<int> A_dim( A->data_dim.begin() + A->data_dim.size() - 2, A->data_dim.end() );
-		std::vector<int> B_dim( B->data_dim.begin() + B->data_dim.size() - 2, B->data_dim.end() );
-
-		int32_t rows = A_dim[0];
-		int32_t cols = B_dim[1];
-		int32_t inner = A_dim[1];
-		int32_t inner2 = B_dim[0];
-		if( inner == 0 ) inner=1;
-
-		// TODO: handle the case of [N] * [Nx1] multiplication,
-		//       i.e. shift rows to inner, set rows as 1
-		//       and similarly, the case of input[1] being a 1D vector 
-		if( inner != inner2 )
-			ERROR("MatMul input's inner dimensions don't match");
-
-		bool A_is_2d = A->data_dim.size() == 2;
-		bool B_is_2d = B->data_dim.size() == 2;
-
-		if ( A_is_2d && B_is_2d )
-		{
-			INDT_1 << "/* MatMul */" << std::endl;
-			INDT_1 << "uint32_t r, c, i;" << std::endl;
-			INDT_1 << "for( r=0; r<" << rows << "; r++ )" << std::endl;
-			INDT_2 << "for( c=0; c<" << cols << "; c++ ) {" << std::endl;
-			INDT_3 << "Y[r][c] = 0;" << std::endl;
-			INDT_3 << "for( i=0; i<" << inner << "; i++ )" << std::endl;
-			INDT_4 << "Y[r][c] += A[r][i] * B[i][c];" << std::endl;
-			INDT_2 << "}" << std::endl;
-		}
-		else
-		{
-			std::string A_txt = A_is_2d ? "A" : "A[n]";
-			std::string B_txt = B_is_2d ? "B" : "B[n]";
-
-			INDT_1 << "/* MatMul */" << std::endl;
-
-			INDT_1 << "for( uint32_t n=0; n<" << A->data_dim[0] << "; n++ ) {" << std::endl;
-
-			INDT_2 << "for( uint32_t r=0; r<" << rows << "; r++ )" << std::endl;
-			INDT_3 << "for( uint32_t c=0; c<" << cols << "; c++ ) {" << std::endl;
-			INDT_4 << "Y[n][r][c] = 0;" << std::endl;
-			INDT_4 << "for( uint32_t i=0; i<" << inner << "; i++ )" << std::endl;
-			INDT_5 << "Y[n][r][c] += " << A_txt << "[r][i] * " << B_txt << "[i][c];" << std::endl;
-			INDT_3 << "}" << std::endl;
-
-			INDT_1 << "}" << std::endl;
-		}
-	} 
-	virtual void resolve(void) override
-	{
-		const Tensor *A = get_input_tensor(0);
-		const Tensor *B = get_input_tensor(1);
-		name_input(0, "A");
-		name_input(1, "B");
-		if(  typeConstraint_highPrecisionNumeric(A) == false )
-			ERROR("Incorrect input for MatMul"); 
-		if(  typeConstraint_highPrecisionNumeric(B) == false )
-			ERROR("Incorrect input for MatMul"); 
-
-		int32_t rows, cols;
-		result_dim(rows, cols);
-
-		Tensor *rv = new Tensor;
-
-		if ( A->data_dim.size() == 3 && B->data_dim.size() == 3 )
-		{
-			if ( A->data_dim[0] != B->data_dim[0] )
-				ERROR( std::string("MatMul input's dimensions don't match: A: ") + vecstr( A->data_dim ) + ", B: " + vecstr( B->data_dim ) );
-
-			rv->data_dim.push_back( A->data_dim[0] );
-		}
-		else if ( A->data_dim.size() == 3 && B->data_dim.size() == 2 )
-		{
-			rv->data_dim.push_back( A->data_dim[0] );
-		}
-		else if ( A->data_dim.size() == 2 && B->data_dim.size() == 3 )
-		{
-			rv->data_dim.push_back( B->data_dim[0] );
-		}
-
-		rv->data_dim.push_back(rows);
-		rv->data_dim.push_back(cols);
-		rv->data_type = A->data_type;
-		register_output(rv, "Y");
-	}
-
-	void result_dim( int32_t &rows, int32_t &cols) const
-	{
-		const Tensor* A = get_input_tensor( 0 );
-		const Tensor* B = get_input_tensor( 1 );
-
-		std::vector<int> A_dim( A->data_dim.begin() + A->data_dim.size() - 2, A->data_dim.end() );
-		std::vector<int> B_dim( B->data_dim.begin() + B->data_dim.size() - 2, B->data_dim.end() );
-
-		// TODO: this is the check for vectors. Check equivalent for N-dimensons: N>2
-		if ( A_dim[1] != 0 && B_dim[1] != 0 )
-		{
-			rows = A_dim[0];
-			cols = B_dim[1];
-		}
-		else if ( A_dim[1] == 0 && B_dim[1] == 0 )
-		{
-			ERROR( "Bad input/unhandled: 2 vectors to MatMul" );
-		}
-		else if ( A_dim[1] == 0 )
-		{
-			cols = B_dim[1];
-			if ( A_dim[0] == B_dim[0] )
-				rows = 1;
-			else
-				rows = A_dim[0];
-		}
-		else
-		{
-			rows = A_dim[0];
-			if ( A_dim[1] == B_dim[0] )
-				cols = 1;
-			else
-				cols = B_dim[0];
-		}
-	}
+	virtual void resolve(void) override;
+	virtual void print(std::ostream &dst) const override;
 };
+
+void MatMul::resolve(void) {
+	Tensor *a = get_input_tensor(0);
+	Tensor *b = get_input_tensor(1);
+
+	assert(a->data_type == b->data_type);
+
+	name_input(0, "A");
+	name_input(1, "B");
+
+	std::vector<int> a_dim = a->data_dim;
+	if (a_dim.size() == 1) {
+		a_dim.insert(a_dim.begin(), 1);
+	} else if (a_dim.size() == 0) {
+		ERROR("Scalar input not supported for MatMul");
+	}
+
+	std::vector<int> b_dim = b->data_dim;
+	if (b_dim.size() == 1) {
+		b_dim.push_back(1);
+	} else if (a_dim.size() == 0) {
+		ERROR("Scalar input not supported for MatMul");
+	}
+
+	assert(a_dim.size() >= 2);
+	assert(b_dim.size() >= 2);
+
+	for (unsigned i = 2; i < a_dim.size() && i < b_dim.size(); i++) {
+		if (a_dim[a_dim.size() - i - 1] != b_dim[b_dim.size() - i - 1]) {
+			ERROR("Invalid broadcast dimensions for MatMul");
+		}
+	}
+
+	std::vector<int> y_dim;
+
+	if (a_dim.size() > b_dim.size()) {
+		y_dim.insert(y_dim.end(), a_dim.begin(), a_dim.end() - 2);
+	} else {
+		y_dim.insert(y_dim.end(), b_dim.begin(), b_dim.end() - 2);
+	}
+	
+	y_dim.push_back(a_dim[a_dim.size() - 2]);
+	y_dim.push_back(b_dim[b_dim.size() - 1]);
+
+	Tensor *y = new Tensor;
+	y->data_dim = a->data_dim;
+	y->data_type = a->data_type;
+	register_output(y, "y");
 }
+
+static std::string MatMul_broadcast(std::string name, const Tensor *t, int to_rank) {
+	assert(t->rank() >= 2);
+
+	std::ostringstream dst;
+	dst << name;
+	for (int i = 0; i < (int)t->data_dim.size() - 2; i++) {
+		dst << "[i" << (to_rank - t->data_dim.size() + i) << "]";
+	}
+	return dst.str();
+}
+
+void MatMul::print(std::ostream &dst) const {
+	INDT_1 << "/* MatMul */" << std::endl;
+
+	Tensor *a = get_input_tensor(0);
+	Tensor *b = get_input_tensor(1);
+	Tensor *y = get_output_tensor(0);
+
+	for (int i = 0; i < (int)y->rank() - 2; i++) {
+		std::string lv = "i" + std::to_string(i);
+		INDT_1 << "for (unsigned " << lv << "=0; " << lv << "<" << y->data_dim[i] << "; " << lv << "++)" << std::endl;
+	}
+
+	std::string a_idx = MatMul_broadcast("A", a, y->rank());
+	if (a->data_dim.size() == 1) {
+		a_idx += "[k]";
+	} else {
+		a_idx += "[i][k]";
+	}
+
+	std::string b_idx = MatMul_broadcast("B", b, y->rank());
+	if (b->data_dim.size() == 1) {
+		b_idx += "[k]";
+	} else {
+		b_idx += "[k][j]";
+	}
+
+	std::string y_idx = MatMul_broadcast("Y", y, y->rank()) + "[i][j]";
+
+	INDT_1 << "{" << std::endl;
+	
+	INDT_2 << "for (unsigned i = 0; i < " << y->data_dim[y->rank() - 2] << "; i++)" << std::endl;
+	INDT_2 << "for (unsigned j = 0; j < " << y->data_dim[y->rank() - 1] << "; j++)" << std::endl;
+	INDT_2 << "{" << std::endl;
+	INDT_2 << y_idx << " = 0;" << std::endl;
+	INDT_3 << "for (unsigned k = 0; k < " << a->data_dim[a->rank() - 1] << "; k++)" << std::endl;
+	INDT_4 << y_idx << " += " << a_idx << " * " << b_idx << ";" << std::endl;
+	INDT_2 << "}" << std::endl;
+
+	INDT_1 << "}" << std::endl;
+}
+
+} // namespace
+
