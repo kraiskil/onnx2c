80b3caffe4938eaf1ca680ff389f340378df0836
Can <can.lehmann@emmtrix.com>
2025-11-11 17:45:51 +0100

    wip

--- src/nodes/qlinearmatmul.h
+++ src/nodes/qlinearmatmul.h
@@ -25,38 +25,37 @@ namespace toC {
                                        const std::string &a_idx,
                                        const std::string &b_idx) const override {
 
-            INDT_4 << "x += ((int32_t)" << a_idx << " - (int32_t)a_zero_point[0]) * "
-                   << "((int32_t)" << b_idx << " - (int32_t)b_zero_point[0]);" << std::endl;
+            INDT_4 << "x += ((int32_t)" << a_idx << " - a_zero_point[0]) * "
+                   << "((int32_t)" << b_idx << " - b_zero_point[0]);" << std::endl;
         }
 
         void print_finalize(std::ostream& dst, const std::string& y_idx) const override {
             std::string float_dtype = get_input_tensor(1)->data_type_str();
             INDT_3 << float_dtype << " scaled = ((" << float_dtype << ")x) * (a_scale[0] * b_scale[0]) / y_scale[0];" << std::endl;
             INDT_3 << "scaled = scaled + (" << float_dtype << ")y_zero_point[0];" << std::endl;
-            INDT_3 << y_idx << " = (" << get_output_tensor(0)->data_type_str() << ") roundf(scaled);" << std::endl;
+            INDT_3 << y_idx << " = (" << get_output_tensor(0)->data_type_str() << ") froundf(scaled);" << std::endl;
         }
 
-        void name_1d_scalar_input(unsigned input_no, std::string name) {
+        void name_scalar_input(unsigned input_no, std::string name) {
             // TODO: Support per-channel quantization
             name_input(input_no, name);
-            if ( get_input_tensor(input_no)->data_dim.size() != 1 ||
-			     get_input_tensor(input_no)->data_dim[0] != 1 ) {
-				ERROR(name << " must be 1 dimensional with 1 element");
+            if (!(get_input_tensor(input_no)->data_dim.size() == 0 ||
+			      (get_input_tensor(input_no)->data_dim.size() == 1 && get_input_tensor(input_no)->data_dim[0] == 1))) {
+				ERROR(name << " must be scalar");
 			}
         }
 
-        void resolve(void) override
-        {
+        void resolve(void) override {
             name_input(0, "A");
-            name_1d_scalar_input(1, "a_scale");
-            name_1d_scalar_input(2, "a_zero_point");
+            name_scalar_input(1, "a_scale");
+            name_scalar_input(2, "a_zero_point");
 
             name_input(3, "B");
-            name_1d_scalar_input(4, "b_scale");
-            name_1d_scalar_input(5, "b_zero_point");
+            name_scalar_input(4, "b_scale");
+            name_scalar_input(5, "b_zero_point");
 
             name_input(6, "y_scale");
-            name_1d_scalar_input(7, "y_zero_point");
+            name_scalar_input(7, "y_zero_point");
 
             Tensor *y_zero_point = get_input_tensor(7);
 
